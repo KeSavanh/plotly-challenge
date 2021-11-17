b74a5be23435f81b6500b4a31de43edcb77928da
Your Name <you@example.com>
2021-11-16 17:22:39 -0800

    Progress

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,12 @@
 
 // Create file path to json file and a variable to hold data loaded
 var filePath = "resources/samples.json";
+var allSampleData;
 // Read sample.json using D3 and store in sampleData
-d3.json(filePath).then(data => {
-    console.log(data);
+d3.json(filePath).then(data => { allSampleData = data
     var dropDownSample = d3.select("#selDataset");
-    data.names.forEach(item => {
-                                    dropDownSample.append("option")
+    allSampleData.names.forEach(item => {
+                                dropDownSample.append("option")
                                                   .text(item)
                                                   .property("value");
                                                          });
@@ -28,23 +28,20 @@ function optionChanged(selId) {
 
 // Demographic Box
 function metaDataBox(selId) { 
-    d3.json(filePath).then(data => {
-        var selSample = data.metadata.filter(item => 
+        var selSample = allSampleData.metadata.filter(item => 
                         item.id.toString() === selId)[0];
         var demoPanel = d3.select("#sample-metadata");
         demoPanel.html("");
         Object.entries(selSample).forEach(([key, value]) => {
             var demoDiplay = (`${key}: ${value}`);
             demoPanel.append('p').text(demoDiplay);
-        })
-    });                                                
+        })                                                
 };
 
 
 // Horizontal bar chart
 function barChart(selId) {
-    d3.json(filePath).then(data => {
-    var selSampleValues = data.samples.filter(sample => sample.id === selId)[0];
+    var selSampleValues = allSampleData.samples.filter(sample => sample.id === selId)[0];
     var otuValues = selSampleValues.sample_values;
     var otuLabels = selSampleValues.otu_labels;
     var otuIds = selSampleValues.otu_ids;
@@ -57,21 +54,21 @@ function barChart(selId) {
     }];
     var layoutBar = {
         title: {
-            text: `Sample No. ${selId}'s Top 10 Operational Taxonomic Units (OTU)`,
+            text: `Sample No. ${selId}'s <br> Top 10 Operational Taxonomic Units (OTU)`,
             font: {
-                size: 20,
-                color: "darkgray"
+                size: 15,
+                color: "black",
+                family: "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif, Helvetica, sans-serif"
+
             }
         }
     };
     Plotly.newPlot('bar', traceBar, layoutBar);
-    });
 };
 
 // Bubble chart
 function bubbleChart(selId) {
-    d3.json("resources/samples.json").then(function(data) {
-    var selSampleValues = data.samples.filter(sample => selId === sample.id.toString())[0];
+    var selSampleValues = allSampleData.samples.filter(sample => selId === sample.id.toString())[0];
     var otuValues = selSampleValues.sample_values;
     var otuLables = selSampleValues.otu_lables;
     var otuIds = selSampleValues.otu_ids;
@@ -96,8 +93,9 @@ function bubbleChart(selId) {
         title: {
             text: `Sample No. ${selId}'s Operational Taxonomic Units (OTU) Volume and Spread`,
             font: {
-                size: 20,
-                color: "darkgray"
+                size: 15,
+                color: "black",
+                family: "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif, Helvetica, sans-serif"
             }
         },
         showlegend: false,
@@ -109,21 +107,26 @@ function bubbleChart(selId) {
         }
     };
     Plotly.newPlot('bubble', traceBubble, layoutBubble);
-    });
 };
 
 // Gauge Chart (BONUS!!!)
 function gaugeChart(selId) {
-    d3.json(filePath).then(function(data) {
-    var selSampleValues = data.metadata.filter(sample => sample.id.toString() === selId);
-    var washFreq = selSampleValues[0].wfreq;
+    var selSampleValues = allSampleData.metadata.filter(sample => sample.id.toString() === selId)[0];
+    var washFreq = selSampleValues.wfreq;
 
     var traceGauge = [{
         domain: { x: [0, 1], y: [0, 1]},
         type: 'indicator',
         mode: "gauge+number",
         value: washFreq,
-        title: { text: `Sample No. ${selId} Belly Button Weekly Washing Frequency <br>Scrubs per Week`},
+        title: { 
+            text: `Sample No. ${selId} Belly Button Weekly Washing Frequency <br>Scrubs per Week`,
+            font: {
+                size: 15,
+                color: "black",
+                family: "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif, Helvetica, sans-serif"
+            }
+        },
         gauge: {
             axis: { range: [null, 9]},
             bar: { color: "#ff8457"},
@@ -145,7 +148,6 @@ function gaugeChart(selId) {
         height: 350,
     };
     Plotly.newPlot('gauge', traceGauge, layoutGauge);
-    });
 };
 
 
